84f4d85e4505a4a4b9f258d33ec3aeae5eb8a9dd
John Waweru <wambugu.john.waweru@outlook.com>
2019-05-12 17:16:37 +0300

    added required routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserCardComponent } from './user-card/user-card.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'users',
+    component: UserListComponent,
+  },
+  {
+    path: 'users/:id',
+    component: UserCardComponent,
+  },
+  { path: '',
+    redirectTo: '/users',
+    pathMatch: 'full'
+  },
+  { path: '**', component: UserListComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(
+    routes,
+    { enableTracing: true, scrollPositionRestoration: 'enabled' }
+  )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
